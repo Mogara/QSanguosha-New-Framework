12902ae0295efe1d626f2ef5d12bf768cc7a79ea
unknown <takashiro@mogara.org>
2015-08-23 17:15:35 +0800

    [0.0.0][enhancement][0][Use lower-case source directories and remove duplicated scripts][client|GUI]

--- src/utility.js
+++
@@ -1,9 +0,0 @@
-.pragma library
-
-Array.prototype.contains = function(element) {
-    for (var i = 0; i < this.length; i++) {
-        if (this[i] === element)
-            return true;
-    }
-    return false;
-}

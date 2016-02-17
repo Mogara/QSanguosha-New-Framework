51ba76e2e7cba86bf5d545aade1d9dfb2eef648c
Takashiro <takashiro@mogara.org>
2016-02-17 16:04:52 +0800

    Fix a bug of room settings

--- src/server/roomsettings.h
+++ src/server/roomsettings.h
@@ -20,7 +20,10 @@
 #ifndef ROOMSETTINGS_H
 #define ROOMSETTINGS_H
 
-#include <CRoomSettings>
+/* The original header file must be included here, for it's perhaps a bug of moc that no properties of the
+ * parent class will be generated in QMetaObject otherwise.
+*/
+#include <croomsettings.h>
 
 class RoomSettings : public CRoomSettings
 {

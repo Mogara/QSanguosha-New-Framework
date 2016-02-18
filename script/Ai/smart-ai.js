38612b49b65eebc217e85a99bfe834a2edf31b9b
Fs <Fsu0413@gmail.com>
2016-02-18 08:19:39 +0800

    test of AI functions

--- script/Ai/smart-ai.js
+++ script/Ai/smart-ai.js
@@ -1,19 +1,20 @@
 function init()
 {
+	aiEngineFunctions.notifyToRobot(6, "init")
 	return true;
 }
 
 function request(command, data)
 {
-	
+	aiEngineFunctions.notifyToRobot(6, "requested")
 }
 
 function reply(command, data)
 {
-	
+	aiEngineFunctions.notifyToRobot(6, "replyed")
 }
 
 function notify(command, data)
 {
-	
+	aiEngineFunctions.notifyToRobot(6, "notified")
 }
